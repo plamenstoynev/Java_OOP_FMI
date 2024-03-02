3170a046670b5a3db98e9cebcda3b1dcf12dd29d
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-03-02 10:26:41 +0200

    Add RectangleTest.java

--- Rectangle/RectangleTest.java
+++ Rectangle/RectangleTest.java
@@ -1,7 +1,7 @@
 package Rectangle;
 
 public class RectangleTest {
-    static void main (String[] args){
+    public static void main (String[] args){
         Rectangle r = new Rectangle(2, 3, "blue");
         System.out.println(r.getArea());
         System.out.println(r.getPerimeter());
